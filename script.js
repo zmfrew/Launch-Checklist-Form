a7966be02bffb818b1b8136628e6424b446b71a1
Zach Frew <zachary.frew@ascension-external.org>
2020-03-18 10:41:10 -0500

    Show list of faulty items on submit

--- script.js
+++ script.js
@@ -12,19 +12,64 @@
 <img src="${}">
 */
 let cargoMass
+let cargoStatus
 let copilotName
+let copilotStatus
+let faultyItems
 let form
 let fuelLevel
+let fuelStatus
+let launchStatus
 let pilotName
+let pilotStatus
 
 function init() {
    cargoMass = document.querySelector('input[name=cargoMass]')
+   cargoStatus = document.getElementById('cargoStatus')
    copilotName = document.querySelector('input[name=copilotName]')
+   copilotStatus = document.getElementById('copilotStatus')
+   faultyItems = document.getElementById('faultyItems')
    form = document.getElementById('launchForm')
    fuelLevel = document.querySelector('input[name=fuelLevel]')
+   fuelStatus = document.getElementById('fuelStatus')
+   launchStatus = document.getElementById('launchStatus')
    pilotName = document.getElementById('pilotName')
+   pilotStatus = document.getElementById('pilotStatus')
 
    form.addEventListener('submit', validate)
+}
+
+function checkFaultyItems(event, cargo, fuel) {
+   if (fuel < 10000) {
+      shuttleNotReady(event, fuelStatus, "Not enough fuel for the journey!")
+   }
+
+   if (cargo > 10000) {
+      shuttleNotReady(event, cargoStatus, "There's too much mass for the shuttle to take off!")
+   }
+
+   if (cargo <= 10000 && fuel >= 10000) {
+      console.log('we ready')
+      shuttleIsReady(event)
+   }
+}
+
+function shuttleIsReady(event) {
+   event.preventDefault()
+   cargoStatus.innerHTML = "Cargo mass low enough for launch"
+   copilotStatus.innerHTML = "Co-pilot Ready"
+   fuelStatus.innerHTML = "Fuel level high enough for launch"
+   pilotStatus.innerHTML = "Pilot Ready"
+   launchStatus.innerHTML = "Shuttle is ready for launch"
+   launchStatus.style.color = 'green'
+}
+
+function shuttleNotReady(event, element, message) {
+   event.preventDefault()
+   faultyItems.style.visibility = 'visible'
+   element.innerHTML = message
+   launchStatus.innerHTML = "Shuttle not ready for launch"
+   launchStatus.style.color = 'red'
 }
 
 function validate(event) {
@@ -33,6 +78,9 @@ function validate(event) {
       isNaN(fuelLevel.value) || pilotName.value === "") {
       alert("All fields are required!")
       event.preventDefault()
+   } else {
+      console.log('here')
+      checkFaultyItems(event, Number(cargoMass.value), Number(fuelLevel.value))
    }
 }
 
