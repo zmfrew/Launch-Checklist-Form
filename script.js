47610e61940ed4e44109c0abfbd0c91069fb0155
Zach Frew <zachary.frew@ascension-external.org>
2020-03-17 23:40:05 -0500

    Add validation logic for inputs

--- script.js
+++ script.js
@@ -11,3 +11,29 @@
 </ol>
 <img src="${}">
 */
+let cargoMass
+let copilotName
+let form
+let fuelLevel
+let pilotName
+
+function init() {
+   cargoMass = document.querySelector('input[name=cargoMass]')
+   copilotName = document.querySelector('input[name=copilotName]')
+   form = document.getElementById('launchForm')
+   fuelLevel = document.querySelector('input[name=fuelLevel]')
+   pilotName = document.getElementById('pilotName')
+
+   form.addEventListener('submit', validate)
+}
+
+function validate(event) {
+   if (cargoMass.value === "" || isNaN(cargoMass.value) ||
+      copilotName.value === "" || fuelLevel.value === "" ||
+      isNaN(fuelLevel.value) || pilotName.value === "") {
+      alert("All fields are required!")
+      event.preventDefault()
+   }
+}
+
+window.onload = init
